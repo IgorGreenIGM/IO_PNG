ff7448786cf16958cc5572a2a28642664e6cf7f2
Mr.G <107557907+IgorGreenIGM@users.noreply.github.com>
2023-01-26 10:39:55 +0100

    partial changes
    > save() method can now several accept compression mode
    > added enum for differents compress modes on save() method
    > readPixels() method only read pixels(not yet pHYs chunk data)

--- include/PNG/PNG.h
+++ include/PNG/PNG.h
@@ -1,13 +1,15 @@
 #ifndef _PNG_H_INCLUDED_
 #define _PNG_H_INCLUDED_
 
+#include <map>
 #include <cmath>
 #include <vector>
 #include <cstdio>
 #include <fstream>
-#include <GL/gl.h>
 #include <iostream>
 #include <stdexcept>
+
+#include "../zlib/zlib.h"
 
 #include "Chunks/IHDR_CHUNK.h"
 #include "Chunks/PHYS_CHUNK.h"
@@ -25,7 +27,6 @@ class PNG
         PNG(const PNG &png);
         PNG(const std::string &path);
         PNG(const uint8_t *pixelBuffer, int s_width, int s_height, int bitDepth, int colorMode);
-        PNG(const uint8_t *pixelBuffer, int s_width, int s_height, int bitDepth, int colorMode, unsigned int ppuX, unsigned int ppuY, uint8_t unitSpecifier);
         ~PNG();
 
         // accessors
@@ -37,10 +38,15 @@ class PNG
 
         uint8_t *get_raw_pixels() const;
 
-        void save(const std::string &path);
-        static void glScreenshot(const std::string &png_dir, int x, int y, int width, int height, int bitDepth, int colorMode, int colorChannel, unsigned int ppuX, unsigned int ppuY, uint8_t unitSpecifier);
+        void save(const std::string &path, int compress_mode = COMPRESS::DEFAULT);
 
         PNG &operator=(const PNG &png_src);
+        
+        /**
+         * @brief output compression modes, according to zlib-defalte() modes
+         * 
+         */
+        enum COMPRESS{BEST = Z_BEST_COMPRESSION, SPEED = Z_BEST_SPEED, DEFAULT = Z_DEFAULT_COMPRESSION, NO = Z_NO_COMPRESSION};
 
     private : 
         uint8_t *m_signature = nullptr; /**< the default signature of all PNG files*/
@@ -53,7 +59,7 @@ class PNG
         IEND_CHUNK *m_IEND = nullptr;
         
         uint8_t *unfilter_line(const uint8_t *line_in, int lineLength, uint8_t filterMode, bool is_prev_line, const uint8_t *unfiltered_prev_line, uint8_t colorChannel);
-        uint8_t *readPixels(const std::string &path, int &s_width, int &s_height, uint8_t &bitDepth, uint8_t &colorMode, uint8_t &colorChannel, int &pixelsBufferLen, int &ppuX, int &ppuY, uint8_t &unitSpecifier);
+        uint8_t *readPixels(const std::string &path, int &s_width, int &s_height, uint8_t &bitDepth, uint8_t &colorMode, uint8_t &colorChannel, int &pixelsBufferLen);
 };
 
 
